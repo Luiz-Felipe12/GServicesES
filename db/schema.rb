b8b04713593dc03e698d54d58e49eca8bb771ed0
Luiz-Felipe12 <luizfelipe.ssu@gmail.com>
2022-09-23 10:49:22 -0300

    criando o scaffold administrador

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_015221) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_133935) do
+  create_table "administradors", force: :cascade do |t|
+    t.string "nome_completo"
+    t.date "data_de_nascimento"
+    t.string "email"
+    t.string "telefone"
+    t.string "senha"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "clientes", force: :cascade do |t|
     t.string "nome_completo"
     t.date "data_de_nascimento"
