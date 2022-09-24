a43c5c44b7c0422a9fe71ad32a9be3ea6d5088c6
Luiz-Felipe12 <luizfelipe.ssu@gmail.com>
2022-09-24 14:25:19 -0300

    corrigindo alguns validations para se adequarem ao codacy

--- app/models/administrador.rb
+++ app/models/administrador.rb
@@ -1,6 +1,6 @@
 class Administrador < ApplicationRecord
 
-  validates :nome_completo, presence: {message: 'Nome completo obrigatório'}, format: {with: /\A[a-zA-Z]/}, length: {minimum: 8, maximum: 80}
+  validates :nome_completo, presence: {message: 'Nome completo obrigatório'}, format: {with: /\A[a-zA-Z ]/}, length: {minimum: 8, maximum: 80}
   validates :data_de_nascimento, presence: {message: 'Data de nascimento obrigatória'}
   validates :email, presence: {message: 'E-mail obrigatório'}, format: {with: URI:: MailTo::EMAIL_REGEXP}, uniqueness: true
   validates :telefone, presence: {message: 'Telefone para contato obrigatório'}, length: {is: 11, message: 'Precisa ter 11 dígitos'}
@@ -9,7 +9,7 @@ class Administrador < ApplicationRecord
   validate :valida_data
 
   def valida_data
-    if data_de_nascimento.present? && data_de_nascimento > Date.today
+    if data_de_nascimento.present? && data_de_nascimento > Time.zone.today
       errors.add(:data_de_nascimento, "A data deve ser a de hoje ou anterior")
     end
   end
