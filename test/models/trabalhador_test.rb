516f9c16c2948c41a85d8a23ee5fb8d41d53c570
Giclayne <giclayne@gmail.com>
2022-09-26 23:26:24 -0300

    teste model trabalhador

--- test/models/trabalhador_test.rb
+++ test/models/trabalhador_test.rb
@@ -4,4 +4,22 @@ class TrabalhadorTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @trabalhador = Trabalhador.new(nome_completo:'Jose Barbosa Silva', data_de_nascimento: Time.zone.today,email:'josebarbosa@gmail.com',telefone:'87987654321', profissao:'Auxiliar de Limpeza', senha:'12345678')
+  end
+
+  test 'trabalhador valido' do
+    assert @trabalhador.valid?
+  end
+
+  test 'trabalhador sem nome' do
+    @trabalhador.nome_completo = " "
+    assert_not @trabalhador.valid?
+  end
+
+  test 'trabalhador sem profissao' do
+    @trabalhador.profissao = " "
+    assert_not @trabalhador.valid?
+  end
 end
