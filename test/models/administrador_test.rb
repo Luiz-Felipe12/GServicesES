5b3f86060ecb857d238f42391ac1611301e14de8
Hester910 <hadassa.ester@ufrpe.br>
2022-10-08 16:23:17 -0300

    Refactoring - Extract Method - teste model administrador - salvando administrador com dados validos

--- test/models/administrador_test.rb
+++ test/models/administrador_test.rb
@@ -2,15 +2,15 @@ require "test_helper"
 
 class AdministradorTest < ActiveSupport::TestCase
 
+  def setup
+    @administrador = Administrador.new(nome_completo: 'Luiz Felipe Ferr',
+                                       data_de_nascimento: Time.zone.today,
+                                       email: 'luizfelipeghg.ssu@gmail.com',
+                                       telefone:'40028922222',
+                                       senha:'123456789')
+  end
   test 'Salvando administrador com dados validos' do
-    administrador = Administrador.new nome_completo: 'Luiz Felipe Ferr',
-                        data_de_nascimento: Time.zone.today,
-                        email: 'luizfelipeghg.ssu@gmail.com',
-                        telefone:'40028922222',
-                        senha:'123456789'
-
-
-    assert administrador.save
+    assert @administrador.valid?
 
   end
 
